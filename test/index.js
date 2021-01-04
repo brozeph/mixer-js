d719b54ce74133eb727d2b7119d58ea5cb50d8ed
Joshua Thomas <brozeph@Joshuas-MacBook-Pro.local>
2021-01-04 10:53:07 -0800

    initial commit, project setup w/Babel and Mocha/Chai tests

---
+++ test/index.js
@@ -0,0 +1,9 @@
+import 'chai/register-should';
+
+describe('mixer-js', async () => {
+  describe('#init()', async () => {
+    it('should accept a list of tracks and filters', async () => {
+      true.should.equal(true);
+    });
+  });
+});
